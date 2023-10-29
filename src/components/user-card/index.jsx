a6d6ad870ad3afd3956fdbc1eb094410cb333d44
tuba evcil <tugbaevcil6060@gmail.com>
2023-10-29 14:48:05 +0300

    right sidebar finished

---
+++ src/components/user-card/index.jsx
@@ -0,0 +1,43 @@
+import Button from "~/components/button";
+import { useState } from "react";
+
+export default function UserCard({ user }) {
+  const [following, setFollowing] = useState(false);
+
+  return (
+    <button className="py-3 px-4 flex gap-3 transition-colors hover:bg-white/[0.03]">
+      <img
+        src={user.avatar}
+        className="w-10 h-10 rounded-full object-cover"
+        alt=""
+      />
+      <div className="flex-1 max-w-full flex flex-col text-left">
+        <div className="text-[15px] text-[#e7e9ea] leading-5 font-bold flex items-center">
+          {user.fullName}
+        </div>
+        <div className="text-[15px] text-[#71767b]">@{user.username}</div>
+      </div>
+      <div>
+        {following ? (
+          <Button
+            variant="white-outline"
+            size="small"
+            onClick={() => setFollowing(false)}
+            className="whitespace-nowrap group"
+          >
+            <div className="flex group-hover:hidden">Following</div>
+            <div className="hidden group-hover:flex">Unfollow</div>
+          </Button>
+        ) : (
+          <Button
+            variant="white"
+            size="small"
+            onClick={() => setFollowing(true)}
+          >
+            Follow
+          </Button>
+        )}
+      </div>
+    </button>
+  );
+}
--- src/layouts/main/rightbar/index.jsx
+++ src/layouts/main/rightbar/index.jsx
@@ -1,12 +1,16 @@
 import Search from "./search";
 import Premium from "../../../components/premium";
 import Topics from "./topics";
+import WhoFollow from "./who-follow";
+import Footer from "./footer";
 export default function RightBar() {
   return (
     <aside className="w-[350px] mr-2.5">
       <Search />
       <Premium />
       <Topics />
+      <WhoFollow />
+      <Footer />
     </aside>
   );
 }
