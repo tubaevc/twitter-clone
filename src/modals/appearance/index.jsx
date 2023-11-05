dc094c4b1144628179ba71452e5d1c5ee29de47a
tuba evcil <tugbaevcil6060@gmail.com>
2023-11-05 14:52:02 +0300

    tab component created

--- src/modals/appearance/index.jsx
+++ src/modals/appearance/index.jsx
@@ -157,6 +157,7 @@ export default function AppearanceModal({ close }) {
                   setBackgroundColor({
                     name: "light",
                     primary: "#fff",
+                    primaryAlpha: "#ffffffa6",
                     secondary: "#f7f9f9",
                     third: "#eff3f4",
                     modal: "#00000066",
@@ -205,6 +206,7 @@ export default function AppearanceModal({ close }) {
                   setBackgroundColor({
                     name: "dark",
                     primary: "#15202b",
+                    primaryAlpha: "#15202ba6",
                     secondary: "#1e2732",
                     third: "#263340",
                     modal: "#5b708366",
@@ -253,6 +255,7 @@ export default function AppearanceModal({ close }) {
                   setBackgroundColor({
                     name: "darker",
                     primary: "#000",
+                    primaryAlpha: "#000000a6",
                     secondary: "#16181c",
                     third: "#212327",
                     modal: "#5b708366",
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,19 @@
-import MainLayout from "../../layouts/main";
+import StickyHeader from "../../components/sticky-header";
+import Tab from "../../components/tab";
 
-export default function Home(){
-    return (
-        <div>
-            home comp
-        </div>
-    )
+export default function Home() {
+  return (
+    <>
+      <Tab activeTab="for-you">
+        <StickyHeader title="Anasayfa">
+          <Tab.Items>
+            <Tab.Item>For you</Tab.Item>
+            <Tab.Item>Following</Tab.Item>
+          </Tab.Items>
+        </StickyHeader>
+        <Tab.Content id="for-you">1. content</Tab.Content>
+        <Tab.Content id="followings">2. content</Tab.Content>
+      </Tab>
+    </>
+  );
 }
